0be7b78d84c88488307755a455f148a320c74dc1
aviglianco <aviglianco22@gmail.com>
2023-10-02 10:07:10 -0300

    Extender test unitario para endpoint estado de jugador

--- tests/test_get_players_status_endpoint.py
+++ tests/test_get_players_status_endpoint.py
@@ -34,6 +34,57 @@ def test_get_player_successful(test_db):
 
 
 @db_session
+def test_get_player_successful_started(test_db):
+    # Test getting a player from a game that has started
+    # Create a game first
+    game_data = {
+        "game": {"name": "Test Game", "min_players": 4, "max_players": 6},
+        "host": {"name": "Test Host"},
+    }
+    client.post("/game/create", json=game_data)
+
+    # Add a few players
+    client.post("/game/join", json={"game_id": 1, "player_name": "Test Player 1"})
+    client.post("/game/join", json={"game_id": 1, "player_name": "Test Player 2"})
+    client.post("/game/join", json={"game_id": 1, "player_name": "Test Player 3"})
+    client.post("/game/join", json={"game_id": 1, "player_name": "Test Player 4"})
+    # Start the game
+    client.post("/game/start", json={"game_id": 1, "player_name": "Test Host"})
+
+    # Get the player
+    response = client.get("/game/1/player/1")
+    assert response.status_code == 200
+    assert response.json() == {
+        "name": "Test Host",
+        "owner": True,
+        "id": 1,
+        "table_position": 1,
+        "role": None,
+        "alive": True,
+        "quarantine": False,
+        "hand": [],
+    }
+
+
+@db_session
+def test_get_player_started_not_owner(test_db):
+    # Get the player
+    response = client.get("/game/1/player/3")
+    assert response.status_code == 200
+    assert response.json() == {
+        "name": "Test Player 2",
+        "owner": False,
+        "id": 3,
+        "table_position": 3,
+        "role": None,
+        "alive": True,
+        "quarantine": False,
+        "hand": [],
+    }
+    rollback()
+
+
+@db_session
 def test_get_player_nonexistent(test_db):
     # Test getting a player that doesn't exist
     # Get a player that doesn't exist
