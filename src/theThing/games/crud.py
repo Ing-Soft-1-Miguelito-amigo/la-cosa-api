9be584f393cf89ef9f8d8aa229490a63be3b88dd
Adolfo <banchioadolfo@gmail.com>
2023-10-23 14:00:10 -0300

    IS1MA-123 Arreglar update game

--- src/theThing/games/crud.py
+++ src/theThing/games/crud.py
@@ -165,7 +165,40 @@ def update_game(game_id: int, game: schemas.GameUpdate):
         game_to_update = models.Game[game_id]
         game_to_update.set(**game.model_dump(exclude_unset=True))
         game_to_update.flush()
-        response = schemas.GameInDB.model_validate(game_to_update)
+        if game_to_update.turn is not None:
+            played_card = None
+            response_card = None
+            destination_player = ""
+
+            if game.turn.played_card is not None:
+                played_card = models.Card[game.turn.played_card]
+            if game.turn.response_card is not None:
+                response_card = models.Card[game.turn.response_card]
+            if game.turn.destination_player is not None:
+                destination_player = game.turn.destination_player
+
+            return_turn = schemas.TurnOut(
+                owner=game.turn.owner,
+                played_card=played_card,
+                destination_player=destination_player,
+                response_card=response_card,
+                state=game.turn.state,
+            )
+
+            return_game = schemas.GameInDB(
+                id=game.id,
+                name=game.name,
+                min_players=game.min_players,
+                max_players=game.max_players,
+                state=game.state,
+                play_direction=game.play_direction,
+                turn=return_turn,
+                players=game.players,
+                deck=game.deck,
+            )
+            return return_game
+        else:
+            response = schemas.GameInDB.model_validate(game)
     return response
 
 
